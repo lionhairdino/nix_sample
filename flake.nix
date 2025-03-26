166c571fc2b28aa000a0a5336f0622e82026a64a
Ji-Haeng Huh <jhhuh.note@gmail.com>
2025-03-26 15:06:36 +0900

    add flake.nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,27 @@
 
   inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-24.11;
 
+
+  # TODO: Use `flake-parts` or `nix-utils`
+  # 1. override:  "self: super: { ... }"
+  # 2. module: ...
+  # haskell.nix sucks !!!
+  # horizon haskell ??
+  # nix + bazel (cabal XXX) -- bazel rule (TWEAG)
+  # nix + buck2 (!!!)
+
   outputs = inputs:
   let
     system = "x86_64-linux";
     pkgs = import inputs.nixpkgs { inherit system; };
+    simplist = pkgs.callPackage ./simplist {};
   in
     {
 
-      packages.${system}.default =  null;
+      packages.${system} = {
+       inherit simplist;
+       default = simplist;
+     };
 
     };
 
